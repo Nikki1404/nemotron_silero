6b343aa6b093cc367db0151ae90e95702585c417
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-26 22:34:09 +0530

    Update updqates.py

--- updqates.py
+++ updqates.py
@@ -42,6 +42,6 @@ RUN python3 -m pip install --no-cache-dir -r requirements.txt
 COPY main.py /app
 COPY router.py /app
 
-EXPOSE 8080
+EXPOSE 8081
 
-CMD ["python3", "-m", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
+CMD ["python3", "-m", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8081"]
