67c53eed39960c76f1857791b753ec9e26bc6034
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-03-02 16:21:29 +0530

    Create updates.py

---
+++ updates.py
@@ -0,0 +1,9 @@
+(nemo_env) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/nemotron_silero/benchmarking# aws configure
+AWS Access Key ID [****************I2I"]: ASIAQ3EGP2D5WOYZEI2I
+AWS Secret Access Key [****************F+i"]: VWZgGWwB7B4tOz9M89gNiVfs801HcqAdidWp9F+i
+AWS Session Token [None]: IQoJb3JpZ2luX2VjELv//////////wEaCXVzLWVhc3QtMSJGMEQCIHsg6Zlj/S5jO9KQMOLO932b9LX6VJ9p6LkEYIVQte<rGnQD5NADw33+wc4fp27ydnDRUzch8F3ktxU+cQTOg/Zsi6PwyeH6INEhSpV0kV70KDfz7MomT1JC7zX/3bLg==
+Default region name [us-east-1]:
+Default output format [None]:
+(nemo_env) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/nemotron_silero/benchmarking# aws s3 ls s3://cx-speech
+
+SSL validation failed for https://cx-speech.s3.us-east-1.amazonaws.com/?list-type=2&prefix=&delimiter=%2F&encoding-type=url [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: self signed certificate in certificate chain (_ssl.c:1032)
