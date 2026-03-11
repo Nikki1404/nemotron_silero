62b436f9bec75c3a97843bf9f65be7324843c992
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-03-11 16:29:41 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,10 +1,3 @@
-unset http_proxy
-unset https_proxy
-export the keys for AWS
-kubectl scale deployment asr-realtime-custom-vad -n cx-speech --replicas=0
-kubectl set image deployment/asr-realtime-custom-vad asr-realtime-custom-vad-container=058264113403.dkr.ecr.us-east-1.amazonaws.com/cx-speech/asr-realtime-custom-vad:0.0.19 -n cx-speech
-kubectl scale deployment asr-realtime-custom-vad -n cx-speech --replicas=1
-Test to see if it is working fine, and if it does not work then revert to the original version by running the following commands:
-kubectl scale deployment asr-realtime-custom-vad -n cx-speech --replicas=0
-kubectl set image deployment/asr-realtime-custom-vad asr-realtime-custom-vad-container=058264113403.dkr.ecr.us-east-1.amazonaws.com/cx-speech/asr-realtime-custom-vad:0.0.16 -n cx-speech
-kubectl scale deployment asr-realtime-custom-vad -n cx-speech --replicas=1
+kubectl logs -f  deployment/asr-realtime-custom-vad -n cx-speech
+Error from server (BadRequest): container "asr-realtime-custom-vad-container" in pod "asr-realtime-custom-vad-69677989f6-nwx6t" is waiting to start: ContainerCreating
+
