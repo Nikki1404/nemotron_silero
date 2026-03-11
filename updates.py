192113c3a67c810fc069d7587412485a2ea1a155
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-03-11 16:44:29 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,1 +1,10 @@
-kubectl get nodes -o=custom-columns=NAME:.metadata.name,GPUs:.status.allocatable.nvidia\.com/gpu
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# kubectl get nodes -o=custom-columns=NAME:.metadata.name,GPUs:.status.allocatable.nvidia\.com/gpu
+NAME                            GPUs
+ip-10-90-114-104.ec2.internal   <none>
+ip-10-90-114-153.ec2.internal   <none>
+ip-10-90-114-166.ec2.internal   <none>
+ip-10-90-114-186.ec2.internal   <none>
+ip-10-90-114-72.ec2.internal    <none>
+ip-10-90-114-87.ec2.internal    <none>
+ip-10-90-126-146.ec2.internal   <none>
+ip-10-90-126-19.ec2.internal    <none>
